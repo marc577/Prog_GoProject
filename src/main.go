3a90ef8fd7be110c36e6ca22d26a429db9493f03
Marc <marc@h8m.eu>
2018-10-30 10:36:57 +0100

    working tls example

--- src/main.go
+++ src/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"crypto/tls"
 	"net"
 	"bufio"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	var webPort int = 443
 	log.SetFlags(log.Lshortfile)
 
-	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	cer, err := tls.LoadX509KeyPair("keys/server.crt", "keys/server.key")
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	fmt.Println("Keys loaded!")
 
 	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	ln, err := tls.Listen("tcp", ":443", config)
+	ln, err := tls.Listen("tcp", joinStr(":", strconv.Itoa(webPort)), config)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	fmt.Println(joinStr("Allocated Port ",strconv.Itoa(webPort)))
 	defer ln.Close()
 
 	for {
@@ -36,6 +42,7 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	fmt.Println("New Connection")
 	r := bufio.NewReader(conn)
 	for {
 		msg, err := r.ReadString('\n')
@@ -52,4 +59,12 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 	}
+}
+
+func joinStr(strs ...string) string {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(str)
+	}
+	return sb.String()
 }
